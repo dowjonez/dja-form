373ebb715b6fc2c1c29822993a8a22a7057628c8
Chris Strong <chris-strong@am.com>
2018-12-09 06:11:39 -0600

    bucket name being used in place of bucket region :: resolved

--- src/app/services/s3.service.ts
+++ src/app/services/s3.service.ts
@@ -13,32 +13,36 @@ export class S3Service {
   public aws_bucket = 'djafricashow-submissions';
   public bucketRegion = 'us-east-1';
   public IdentityPoolId = 'us-east-1:72e65e24-58b0-4d99-baa5-e35acdeb6d78';
-
   public s3 : AWS.S3;
 
   constructor() { 
-    // Initialize the Amazon Cognito credentials provider
-    
-    AWS.config.update({
-      region: this.aws_bucket,
-      credentials: new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: this.IdentityPoolId
-      })
+    let creds : AWS.CognitoIdentityCredentials =  new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: this.IdentityPoolId
     });
-    
+
+    // Initialize the Amazon Cognito credentials provider
+    AWS.config.update({
+      region: this.bucketRegion,
+      credentials: creds
+    });
+
+    AWS.config.getCredentials( err => {
+      debugger  
+    } )
+
+   
+
+    console.log(AWS.config.credentials);
+
     this.s3 = new AWS.S3({
       apiVersion: '2006-03-01',
       params: {Bucket:this.aws_bucket}
     });
-
-    console.log(this.s3);
-
-    
   }
 
 
   public postFile( file ){
-      var params = {
+      let params = {
         Bucket: this.aws_bucket, 
         Key: file.name, 
         Body: file,
@@ -46,9 +50,7 @@ export class S3Service {
         ContentType: file.mimetype
       };
       
-      var result = null;
       
-     
       this.s3.putObject(params, function( err, data ) {
           if( err )
              console.log(err);
